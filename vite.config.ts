bd420ebbecc05fa06064a1dd28a1072c16778cbe
M Anshori <muhammad.anshori@efishery.com>
2024-03-03 06:33:40 +0700

    fix: conflict

--- vite.config.ts
+++ vite.config.ts
@@ -39,24 +39,5 @@ export default defineConfig({
   build: {
     manifest: true,
     outDir: './dist', // for user easy to use, vercel use default dir -> dist
-    rollupOptions: {
-      output: {
-        manualChunks: (id: string) => {
-          if (id.includes('node_modules')) {
-            return 'vendors';
-            // If there will be more and more external packages referenced in the future,
-            // the following approach can be considered.
-            // const name = id.split('node_modules/')[1].split('/');
-            // return name[0] == '.pnpm' ? name[1] : name[0];
-          } else {
-            for (const item of individuallyPackages) {
-              if (id.includes(item)) {
-                return item;
-              }
-            }
-          }
-        },
-      },
-    },
   },
 });
